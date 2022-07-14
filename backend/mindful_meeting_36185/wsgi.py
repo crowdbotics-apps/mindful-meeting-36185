99ca1c2d9990fff3e6b622c7ef9ff0741e1badd2
Crowdbotics <team@crowdbotics.com>
2022-07-14 16:40:34 +0000

    Initial commit.

---
+++ backend/mindful_meeting_36185/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for mindful_meeting_36185 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mindful_meeting_36185.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "mindful_meeting_36185",
+  url: "https://mindful_meeting_36185.botics.co",
+  api: "https://mindful_meeting_36185.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
